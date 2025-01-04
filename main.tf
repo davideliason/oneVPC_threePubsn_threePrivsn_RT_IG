1e0d8ca8cd5c5082bbea9c27e0e8bffc12aa444e
David Eliason <davethemaker@gmail.com>
2025-01-04 13:32:15 -0800

    use Copilot to create three public subnets

--- main.tf
+++ main.tf
@@ -7,3 +7,34 @@ terraform {
   }
 }
 
+resource "aws_vpc" "prd-vpc" {
+  cidr_block = "10.0.0.0/16"
+}
+
+resource "aws_subnet" "prd-pub-subnet-1 " {
+  vpc_id     = aws_vpc.prd-vpc.id
+  cidr_block = "10.0.0.0/24"
+
+  tags = {
+    Name = "prd-pub-subnet-1"
+  }
+}
+
+resource "aws_subnet" "prd-pub-subnet-2" {
+  vpc_id     = aws_vpc.prd-vpc.id
+  cidr_block = "10.0.1.0/24"
+
+  tags = {
+    Name = "prd-pub-subnet-2"
+  }
+}
+
+resource "aws_subnet" "prd-pub-subnet-3" {
+  vpc_id     = aws_vpc.prd-vpc.id
+  cidr_block = "10.0.2.0/24"
+
+  tags = {
+    Name = "prd-pub-subnet-3"
+  }
+}
+
