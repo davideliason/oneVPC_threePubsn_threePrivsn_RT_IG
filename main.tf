095cfc26bfdec358e448c18fe00a1f6e63516e80
David Eliason <davethemaker@gmail.com>
2025-01-04 13:34:40 -0800

    add AZ to the subnets

--- main.tf
+++ main.tf
@@ -12,8 +12,9 @@ resource "aws_vpc" "prd-vpc" {
 }
 
 resource "aws_subnet" "prd-pub-subnet-1 " {
-  vpc_id     = aws_vpc.prd-vpc.id
-  cidr_block = "10.0.0.0/24"
+  vpc_id            = aws_vpc.prd-vpc.id
+  cidr_block        = "10.0.0.0/24"
+  availability_zone = "us-west-2a"
 
   tags = {
     Name = "prd-pub-subnet-1"
@@ -21,8 +22,9 @@ resource "aws_subnet" "prd-pub-subnet-1 " {
 }
 
 resource "aws_subnet" "prd-pub-subnet-2" {
-  vpc_id     = aws_vpc.prd-vpc.id
-  cidr_block = "10.0.1.0/24"
+  vpc_id            = aws_vpc.prd-vpc.id
+  cidr_block        = "10.0.1.0/24"
+  availability_zone = "us-west-2b"
 
   tags = {
     Name = "prd-pub-subnet-2"
@@ -30,8 +32,9 @@ resource "aws_subnet" "prd-pub-subnet-2" {
 }
 
 resource "aws_subnet" "prd-pub-subnet-3" {
-  vpc_id     = aws_vpc.prd-vpc.id
-  cidr_block = "10.0.2.0/24"
+  vpc_id            = aws_vpc.prd-vpc.id
+  cidr_block        = "10.0.2.0/24"
+  availability_zone = "us-west-2c"
 
   tags = {
     Name = "prd-pub-subnet-3"
