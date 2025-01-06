226e7878c57cc702d625152acada84fcdbb63a71
David Eliason <davethemaker@gmail.com>
2025-01-06 03:23:59 -0800

    created bastion host for access to nat gateway resource that allows the ec2 instance within the private subnet to access the internet

--- main.tf
+++ main.tf
@@ -40,26 +40,92 @@ resource "aws_security_group" "prd-web-sg" {
     protocol    = "tcp"
     cidr_blocks = ["10.0.0.0/16"] # allow traffic from the internet
   }
+
+  ingress {
+    description = "SSH"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"] # allow traffic from the internet
+  }
+
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"] # allow all traffic to the internet
+  }
 }
+
+
+
+# create NAT gateway for private subnet
+resource "aws_eip" "nat-gw-eip" {
+  vpc = true
+}
+
+resource "aws_nat_gateway" "nat-gw" {
+  allocation_id = aws_eip.nat-gw-eip.id
+  subnet_id     = aws_subnet.prd-pub-subnet-1.id
+}
+
 
 
 # create t2.micro ec2 instance for development web server in us-west-2a
 resource "aws_instance" "prd-web-server" {
-  ami                    = "ami-07d9cf938edb0739b"
-  instance_type          = "t2.micro"
-  subnet_id              = aws_subnet.prd-pub-subnet-1.id
-  vpc_security_group_ids = [aws_security_group.prd-web-sg.id]
+  ami                         = "ami-07d9cf938edb0739b"
+  instance_type               = "t2.micro"
+  subnet_id                   = aws_subnet.prd-pri-subnet-1.id
+  vpc_security_group_ids      = [aws_security_group.prd-web-sg.id]
+  key_name                    = "best_dir"
+  availability_zone           = "us-west-2a"
+  associate_public_ip_address = true
+
+
+  user_data = <<EOF
+
+  #!/bin/bash
+
+  sudo apt-get update
+
+  sudo apt-get install -y apache2
+
+  sudo systemctl start apache2
+
+  sudo systemctl enable apache2
+
+  echo "Hello from your new Apache server!" > /var/www/html/index.html
+
+  EOF
 
   tags = {
-    Name = "prd-web-server-us-west-2a-public"
+    Name = "prd-web-server-us-west-2a-private"
   }
 }
+
+# create bastion host for ssh access to private subnet
+resource "aws_instance" "bastion-host" {
+  ami                         = "ami-07d9cf938edb0739b"
+  instance_type               = "t2.micro"
+  subnet_id                   = aws_subnet.prd-pub-subnet-1.id
+  vpc_security_group_ids      = [aws_security_group.prd-web-sg.id]
+  key_name                    = "best_dir"
+  availability_zone           = "us-west-2a"
+  associate_public_ip_address = true
+
+  tags = {
+    Name = "bastion-host-us-west-2a-public"
+  }
+}
+
 
 
 resource "aws_subnet" "prd-pub-subnet-1" {
   vpc_id            = aws_vpc.prd-vpc.id
   cidr_block        = "10.0.0.0/24"
   availability_zone = "us-west-2a"
+
 
   tags = {
     Name = "prd-pub-subnet-1"
@@ -153,7 +219,12 @@ resource "aws_route_table_association" "prd-pub-rt-assoc-3" {
 
 resource "aws_route_table" "prd-pri-rt" {
   vpc_id = aws_vpc.prd-vpc.id
+  route {
+    cidr_block = "0.0.0.0/0" # route all traffic to the NAT gateway
+    gateway_id = aws_nat_gateway.nat-gw.id
+  }
 }
+
 
 resource "aws_route_table_association" "prd-pri-rt-assoc-1" {
   subnet_id      = aws_subnet.prd-pri-subnet-1.id
